93237c80db06ba6c7350b8ee1eaafb64b675a880
Rosie Allison-Corio <rosievespa@gmail.com>
2025-03-24 15:02:30 +0000

    added layout

---
+++ src/app/timeline/page.js
@@ -0,0 +1,27 @@
+import pg from "pg"
+
+export default async function Page() {
+
+    const db = new pg.Pool({
+        connectionString: process.env.DB_CONN
+    })
+
+    const allPosts = (await db.query(`SELECT * FROM posts`)).rows
+    console.log(allPosts)
+
+    return (
+        <div className="flex flex-col items-center">
+            <section className="flex flex-col h-[80vh] w-[90vw] bg-slate-700">
+                {allPosts.map((item) => {
+                    <div key={item.id}>
+                        <p>profile picture</p>
+                        <p>{item.content}</p>
+                    </div>
+                })}
+            </section>
+            <section>
+                <button>Create New Post</button>
+            </section>
+        </div>
+    )
+}
