c8c0b45f72c6ba78b5b51f107e38470e3afb1e99
Sufyan <sufyanmalik@hotmail.com>
2025-03-25 10:13:35 +0000

    done cta and footer

---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function CallToAction() {
+  return (
+    <section className="m-4 py-12 bg-orange-600 text-white text-center">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-semibold mb-4">
+          Ready to Join the LocalChord Community?
+        </h2>
+        <p className="text-xl mb-8">
+          Sign up today and start connecting with musicians in your area!
+        </p>
+        <Button asChild>
+          <Link href="/login">Login</Link>
+        </Button>
+      </div>
+    </section>
+  );
+}
--- src/components/LandingPageHeader.jsx
+++ src/components/LandingPageHeader.jsx
@@ -3,14 +3,15 @@ import Link from "next/link";
 export default function LandingPageHeader() {
   return (
     <header className="bg-white shadow-md py-4">
-      <div className="container mx-auto px-4 flex items-center justify-between">
-        <Link
-          href="http://localhost:3000"
-          className="text-2xl font-bold text-orange-600"
-        >
-          LocalChord
-        </Link>
+      <div className="container mx-auto px-5 flex items-center justify-between">
         <nav>
+          <Link
+            href="http://localhost:3000"
+            className="text-2xl font-bold text-orange-600"
+          >
+            LocalChord
+          </Link>
+
           <Link
             href="http://localhost:3000/login"
             className="text-gray-700 hover:text-blue-600"
