b18211197cb253813c2bfabd2190a1619378bbd8
Rosie Allison-Corio <rosievespa@gmail.com>
2025-03-25 21:05:58 +0000

    modal form working, need to test database entry

--- src/utils/actions.js
+++ src/utils/actions.js
@@ -1,19 +1,16 @@
 'use server'
 
 import pg from "pg"
-import { auth } from "@clerk/nextjs/server"
+import { revalidatePath } from "next/cache"
 
-export async function handleSubmit(formData) {
+export async function handlePostDelete({id}) {
 
     const db = new pg.Pool({
         connectionString: process.env.DB_CONN
     })
+  
+    await db.query(`DELETE FROM posts WHERE id = $1`, [id])
 
-    const {userId} = await auth()    
-
-    const data = Object.fromEntries(formData)
-    const {content, thread_id} = data
-    const user_id = await db.query(`SELECT id FROM users WHERE clerk_id = $1`, [userId])
+    revalidatePath('/timeline')
     
-    await db.query(`INSERT INTO posts (user_id, thread_id, content) VALUES ($1, $2, $3)`, [user_id, thread_id, content])
 }
