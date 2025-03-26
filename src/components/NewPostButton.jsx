0a5a10d6a1e2a8caeea78c3da5c35e0b3f5eb2a1
Rosie Allison-Corio <rosievespa@gmail.com>
2025-03-26 13:48:06 +0000

    post form working

--- src/components/NewPostButton.jsx
+++ src/components/NewPostButton.jsx
@@ -10,22 +10,6 @@ export default function NewPostButton({children}) {
         setShowModal(!showModal)
     }
 
-
-    async function handleSubmit(formData) {
-
-        const db = new pg.Pool({
-            connectionString: process.env.DB_CONN
-        })
-
-        const {userId} = await auth()    
-
-        const data = Object.fromEntries(formData)
-        const {content, thread_id} = data
-        const user_id = await db.query(`SELECT id FROM users WHERE clerk_id = $1`, [userId])
-        
-        await db.query(`INSERT INTO posts (user_id, thread_id, content) VALUES ($1, $2, $3)`, [user_id, thread_id, content])
-    }
-
     return (
         <div className="flex flex-col">
             <button className="self-center" onClick={toggleModal}>Make a Post</button>
