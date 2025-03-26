c47796f8bd5e05e065910f65b4ed7a83d046b6f4
Sufyan <sufyanmalik@hotmail.com>
2025-03-26 14:37:13 +0000

    fixed comment form and now also displaying comments under post

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,10 +11,7 @@ import {
 export default function Header() {
   return (
     <nav className="flex justify-between p-4">
-      <Link
-        href="http://localhost:3000"
-        className="text-2xl font-bold text-orange-600"
-      >
+      <Link href="/timeline" className="text-2xl font-bold text-orange-600">
         Ensemble
       </Link>
     </nav>
