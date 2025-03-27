3355d48fcb4b4bed45e23d2f7c353d177d6ec9de
Sufyan <sufyanmalik@hotmail.com>
2025-03-27 12:25:09 +0000

    working sidebar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,20 +8,20 @@ export default {
   ],
   theme: {
   	extend: {
-		fontFamily: {
-			'rye': [
-				'var(--font-rye)',
-				'serif'
-			],
-			'lora': [
-				'var(--font-lora)',
-				'serif'
-			],
-			'inter':[
-				'var(--font-inter)',
-				'sans-serif'
-			]
-		},
+  		fontFamily: {
+  			rye: [
+  				'var(--font-rye)',
+  				'serif'
+  			],
+  			lora: [
+  				'var(--font-lora)',
+  				'serif'
+  			],
+  			inter: [
+  				'var(--font-inter)',
+  				'sans-serif'
+  			]
+  		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
@@ -62,6 +62,16 @@ export default {
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
   			}
   		},
   		borderRadius: {
